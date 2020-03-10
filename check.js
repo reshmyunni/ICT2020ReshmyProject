b6cba5ebaacb06e80a900aa5d96c684d208d1502
reshmyunni <reshmyunni1778@gmail.com>
2020-03-10 10:42:12 +0530

    javascript examples

---
+++ check.js
@@ -0,0 +1,6 @@
+let num = 23;
+if(num%2==0){
+    console.log("Number is even");
+}else{
+    console.log("Number is odd");
+}
---
+++ consoleVal.js
@@ -0,0 +1,7 @@
+var x = 15;
+{
+    let y=23;
+    console.log(y);
+}
+
+console.log(x);
